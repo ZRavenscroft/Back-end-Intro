a5dd17aa510fc1c65d3f107fd92c7c8e77cc7596
ZRavenscroft <110469428+ZRavenscroft@users.noreply.github.com>
2022-11-16 00:03:45 +0400

    Homework

---
+++ Homework(1)/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_1_.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ContentResult Index()
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "<h1>P132</h1>";
+            return result;
+        }
+        public JsonResult Student()
+        {
+            var obj = new { Name = "Zackary", Surname = "Ravenscroft", Age = 20 };
+            JsonResult js = new JsonResult(obj);
+            return js;
+        }
+        public ViewResult About()
+        {
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "about";
+            return viewResult;
+        }
+        public ViewResult Contact()
+        {
+            return View();
+        }
+    }
+}
